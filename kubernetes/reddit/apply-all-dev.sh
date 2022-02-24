ceae6929036416590f3e617deed8c3f3408e1c45
WasaPupkin <wasamyname@gmail.com>
2022-02-25 01:00:18 +0300

    Updated image tags

--- kubernetes/reddit/apply-all-dev.sh
+++ kubernetes/reddit/apply-all-dev.sh
@@ -2,20 +2,20 @@
 kubectl apply -f ./dev-namespace.yml
 # install ingress
 # kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.34.1/deploy/static/provider/cloud/deploy.yaml
-# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingressnginx/controller-v0.34.1/deploy/static/provider/cloud/deploy.yaml
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.1/deploy/static/provider/cloud/deploy.yaml
 
 kubectl apply -n dev -f comment-deployment.yml \
-  -f comment-deployment.yml \
   -f comment-mongo-service.yml \
+  -f comment-service.yml \
   -f mongo-deployment.yml \
   -f mongo-service.yml \
+  -f mongo-network-policy.yml \
   -f post-deployment.yml \
   -f post-service.yml \
   -f post-mongo-service.yml \
   -f ui-deployment.yml \
   -f ui-service.yml \
   -f ui-ingress.yml \
-  -f mongo-network-policy.yml \
   -f persitent-volume.yml \
   -f persitent-volume-claim.yml
 
--- kubernetes/reddit/delete-all-dev.sh
+++ kubernetes/reddit/delete-all-dev.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 kubectl delete -n dev -f comment-deployment.yml \
-  -f comment-deployment.yml \
   -f comment-mongo-service.yml \
+  -f comment-service.yml \
   -f mongo-deployment.yml \
   -f mongo-service.yml \
+  -f mongo-network-policy.yml \
   -f post-deployment.yml \
   -f post-service.yml \
   -f post-mongo-service.yml \
   -f ui-deployment.yml \
   -f ui-service.yml \
   -f ui-ingress.yml \
-  -f mongo-network-policy.yml \
   -f persitent-volume.yml \
   -f persitent-volume-claim.yml
 
